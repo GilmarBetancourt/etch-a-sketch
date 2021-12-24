e4e93dae33a9537e4e1cfc9c27e5ea880958f506
Gilmar Betancourt <gilmarbeta@hotmail.com>
2021-12-23 22:12:14 -0400

    Added functions to change the color of the squares.

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 //Creating the grid
 const container = document.getElementById("container");
 let fullGrid = false;
+let randomColor = false;
+let erase = false;
+let black = false;
 
 document.getElementById("start").addEventListener("click", () => {
   if (fullGrid == true) {
     deleteGrid();
   }
   createGrid();
+  changeSquaresColor();
   fullGrid = true;
 });
 
@@ -18,7 +22,7 @@ function createGrid() {
 
   for (let i = 1; i <= squaresNumber * squaresNumber; i++) {
     const square = document.createElement("div");
-    square.classList = "grid";
+    square.classList = "gridItem";
     container.appendChild(square);
   }
   container.style.gridTemplateColumns = `repeat(${squaresNumber},${rowColumSize}px)`;
@@ -31,3 +35,49 @@ function deleteGrid() {
     container.removeChild(container.lastChild);
   }
 }
+
+//To change the color of the squares on mouseover
+function changeSquaresColor() {
+  document.querySelectorAll(".gridItem").forEach((item) => {
+    item.addEventListener("mouseover", () => {
+      item.style.backgroundColor = "black";
+      if (erase == true) {
+        item.style.backgroundColor = "white";
+      } else if (black == true) {
+        item.style.backgroundColor = "black";
+      } else if (randomColor == true) {
+        //This generates a random color in hex
+        let randomColorCode = Math.floor(Math.random() * 16777215).toString(16);
+        item.style.backgroundColor = `#${randomColorCode}`;
+      }
+    });
+  });
+}
+
+//To identify the button clicked (color selected)
+document.querySelectorAll(".roundButtons").forEach((item) => {
+  item.addEventListener("click", () => {
+    if (item.id == "clearBoard") {
+      //To erase the board
+      document.querySelectorAll(".gridItem").forEach((item) => {
+        item.style.backgroundColor = "white";
+      });
+      console.log(clearBoard);
+    } else if (item.id == "black") {
+      black = true;
+      randomColor = false;
+      erase = false;
+      console.log(black);
+    } else if (item.id == "randomColor") {
+      randomColor = true;
+      black = false;
+      erase = false;
+      console.log(randomColor);
+    } else if (item.id == "erase") {
+      erase = true;
+      randomColor = false;
+      black = false;
+      console.log(erase);
+    }
+  });
+});
